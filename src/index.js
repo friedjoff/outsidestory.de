5d80d94c21a7c371ea328af8ca98b0b7c5928946
Friedjoff Trautwein <mail@friedjoff.de>
2018-02-14 10:15:14 +0100

    Fetch photos from Flickr.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
 import 'normalize.css'
-
-import PhotoSwipe from 'photoswipe'
-import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default.js'
-import 'photoswipe/dist/photoswipe.css'
-import 'photoswipe/dist/default-skin/default-skin.css'
-
-import plyr from 'plyr'
-import 'plyr/dist/plyr.css'
-
 import './style.css'
 
-if (typeof outsidestoryPhotos === 'object') {
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+import Gallery from './gallery'
 
-  var options = {
-    focus: false,
-    modal: false,
-    closeOnScroll: false,
-    history: false,
-    pinchToClose: false,
-    closeOnVerticalDrag: false,
-    barsSize: { top:0, bottom:'auto' },
-    // UI options
-    closeEl: false,
-    shareEl: false,
-    zoomEl: false
-  };
-
-  var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, outsidestoryPhotos, options);
-  gallery.init();
-}
-
-plyr.setup()
+new Gallery('.photos')
